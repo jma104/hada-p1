1257ddfa3d03e6469e50a0a851350cc267a57a03
Javier Martínez Albaladejo <jma104@alu.ua.es>
2019-02-06 08:58:39 +0100

    Added method Seconds2Minutes to class HadaP1

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,8 +6,13 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
-    class Program
+    class HadaP1
     {
+        public static double Seconds2Minutes(double s)
+        {
+            return s / 60;
+        }
+
         static void Main(string[] args)
         {
         }
