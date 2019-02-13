64f7016259855b0b829ef70c8ace26e594531859
Javier Martínez Albaladejo <jma104@alu.ua.es>
2019-02-13 09:11:30 +0100

    Completed main method

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,18 +10,24 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
-            Console.Write("From which unit do you want to start ('m' for minutes, 's' for seconds)? ");
-            string answer=Console.ReadLine();
-            if (answer == "m")
+            do
             {
-                do
+                Console.Write("From which unit do you want to start ('m' for minutes, 's' for seconds)? ");
+                string answer=Console.ReadLine();
+                if (answer == "m")
                 {
                     Console.Write("Enter the amount of minutes: ");
                     double minutes = double.Parse(Console.ReadLine());
-                    Console.WriteLine(minutes + "minutes are " + HadaP1.Minutes2Seconds(minutes) + "seconds.");
-                    Console.Write("Do you want to perform more conversions (y/n)? ");
-                } while (Console.ReadLine()=="y");
-            }
+                    Console.WriteLine(minutes + "minutes are " + HadaP1.Minutes2Seconds(minutes) + " seconds.");
+                }
+                else
+                {
+                    Console.Write("Enter the amount of seconds: ");
+                    double seconds = double.Parse(Console.ReadLine());
+                    Console.WriteLine(seconds + " seconds are " + HadaP1.Minutes2Seconds(seconds) + " minutes.");
+                }
+                Console.Write("Do you want to perform more conversions (y/n)? ");
+            } while (Console.ReadLine()=="y");
         }
     }
 }
