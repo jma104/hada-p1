9840bf6304e793bc4ae5bcd529a2c06fa7d03901
Javier Martínez Albaladejo <jma104@alu.ua.es>
2019-02-13 09:04:15 +0100

    Modified the program so that it asks the user

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,7 +10,18 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
-
+            Console.Write("From which unit do you want to start ('m' for minutes, 's' for seconds)? ");
+            string answer=Console.ReadLine();
+            if (answer == "m")
+            {
+                do
+                {
+                    Console.Write("Enter the amount of minutes: ");
+                    double minutes = double.Parse(Console.ReadLine());
+                    Console.WriteLine(minutes + "minutes are " + HadaP1.Minutes2Seconds(minutes) + "seconds.");
+                    Console.Write("Do you want to perform more conversions (y/n)? ");
+                } while (Console.ReadLine()=="y");
+            }
         }
     }
 }
